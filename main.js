a12225af3095c3729d034c75ea0bfe44a9cb3482
Sieun <si990819@gmail.com>
2022-11-14 21:35:38 +0900

    12_div생성1
    text div안에 content div 생성

--- main.js
+++ main.js
@@ -13,6 +13,16 @@ window.addEventListener('load',()=>{
             return;
         }
         
+        const task_el =document.createElement("div");
+        task_el.classList.add("text");
+
+        const task_content_el=document.createElement("div");
+        task_content_el.classList.add("content");
+
+        task_el.appendChild(task_content_el);
+
+
+
 
         })
 
